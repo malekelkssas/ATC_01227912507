99d09f30110d01250ce7520cf9af5d3cdb19f27d
Malek Mohamed Noureldean Elkssas <malekelkassas444@gmail.com>
2025-05-05 22:51:26 +0300

    chore(errors): add errors base classes

---
+++ BE/src/utils/error/index.ts
@@ -0,0 +1,5 @@
+export * from './AppError.js';
+export * from './NotFoundError.js';
+export * from './UnauthorizedError.js';
+export * from './DatabaseError.js';
+export * from './ValidationError.js';
